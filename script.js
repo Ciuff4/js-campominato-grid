a787e7d60119acfa2cde4a24f378d4da95f2d34d
Ciuff4 <gasperini_cristina@libero.it>
2022-04-22 16:01:42 +0200

    ricreazione logiche: al click compare la base griglia+ funzione reset+fix css pagina

--- script.js
+++ script.js
@@ -5,15 +5,78 @@ con difficoltà 3 => tra 1 e 49
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 */
 
+const wrapper = document.querySelector("main");
 
-/*function boxCreation(griglia) {
-    for(let i=0; i<100; i++){
-        const box=document.createElement("div");
-        box.className("box")
+document.getElementById("bottone").addEventListener("click", start);
 
-    }
-}*/
 
-function randomNumber( max, min){
-    
+function start(){
+    reset(wrapper);
+    const container= document.createElement("div");
+    container.className="cg-container";
+    wrapper.append(container);
 }
+
+
+/**
+ * funzione di reset dell'elemento
+ * @param {*} place 
+ */
+function reset(place) {
+    place.innerHTML=` `;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+
+/*metto i box all'interno del container */
+// console.log(init());
+// // function init(){
+// //     for (let i = 1; i < 100; i++) {
+// //         box= createBox(container,i);
+// //     }
+// // }
+
+
+// console.log(createBox(container));
+// //creo i box
+// function createBox(place,n){
+//     const box=document.createElement("div");
+//     box.className="box";
+//     box.innerHTML=`<span> ${n} </span>`;
+//     place.append(box);
+//     return box;
+// }
+
+
+// function addNumber(num1,num2){
+//     for(let i=0; i<=100; i++){
+        
+//     }
+// }
+// console.log(addNumber());
+
+
+
+/*ogni bomba è un numero random di un array
+se il numero è nell'array allora è una bomba*/
